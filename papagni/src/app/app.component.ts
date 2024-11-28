444839e467438b67c03526b84155acb15b22e6a5
Gabriele Papagni <gabriele.papagni07@gmail.com>
2024-11-28 08:20:48 +0000

    Quarto commit

--- papagni/src/app/app.component.ts
+++ papagni/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SecondComponentComponent, ThirdComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
